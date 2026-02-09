3e6299265defefdc420660328febd1495038e60d
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-09 21:03:09 +0330

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { dataFetcher } from "@/helper/ProductFetcher";
 import Home from "@/templates/Home";
 
-export const revalidate = 1 * 60 * 60 * 24 * 30;
 const page = async () => {
   const data = await dataFetcher();
 
--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -1,10 +1,12 @@
 import { BASE_URL } from "@/api/api";
 import { ProductDetailTypes } from "@/types/products/types";
 
+const revalidate = 1 * 60 * 60 * 24 * 30;
+
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
   try {
     const res: ProductDetailTypes[] = await fetch(`${BASE_URL}/products`, {
-      next: { revalidate: 1 * 60 * 60 * 24 * 30 },
+      next: { revalidate },
     }).then((res) => res.json());
     return res;
   } catch (error) {
