abff290cff767f4c4e29970d3bdc3fd13aa1ce9b
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-10 23:41:22 +0330

    productFetcher changes-2

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -5,17 +5,24 @@ const revalidate = 1 * 60 * 60 * 24 * 30;
 
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
   try {
-    const res: ProductDetailTypes[] = await fetch(`${BASE_URL}/products`, {
-      next: { revalidate },
+    const res = await fetch(`${BASE_URL}/products`, {
+      next: { revalidate: 60 * 60 * 24 * 30 },
       headers: {
         Accept: "application/json",
       },
-    }).then((res) => res.json());
+    });
 
-    return res;
+    // 🔥 VERY IMPORTANT CHECK
+    const contentType = res.headers.get("content-type");
+
+    if (!res.ok || !contentType?.includes("application/json")) {
+      console.error("Invalid API response");
+      return [];
+    }
+
+    return await res.json();
   } catch (error) {
-    const e = error as Error;
-    console.log(e);
+    console.error("Fetch crashed:", error);
     return [];
   }
 };
