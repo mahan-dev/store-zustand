89a0a4b9ae4706bbadddbe929220854c8e0108a1
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-11 21:23:44 +0330

    productFetcher changes-6

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -1,19 +1,16 @@
-import { BASE_URL } from "@/api/api";
+// import { BASE_URL } from "@/api/api";
 import { ProductDetailTypes } from "@/types/products/types";
 
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
-  try {
-    const res: ProductDetailTypes[] = await fetch(`${BASE_URL}/products`, {
+  const res: ProductDetailTypes[] = await fetch(
+    `https://fakestoreapi.com/products`,
+    {
       cache: "no-store",
       headers: {
         Accept: "application/json",
       },
-    }).then((res) => res.json());
+    },
+  ).then((res) => res.json());
 
-    return res;
-  } catch (error) {
-    const e = error as Error;
-    console.log(e);
-    return [];
-  }
+  return res;
 };
