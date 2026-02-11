63ca1f55dedaf622b22c11ea6074b4502ec60a46
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-11 12:46:52 +0330

    productFetcher changes-5

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -1,12 +1,10 @@
 import { BASE_URL } from "@/api/api";
 import { ProductDetailTypes } from "@/types/products/types";
 
-const revalidate = 1 * 60 * 60 * 24 * 30;
-
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
   try {
     const res: ProductDetailTypes[] = await fetch(`${BASE_URL}/products`, {
-      next: { revalidate },
+      cache: "no-store",
       headers: {
         Accept: "application/json",
       },
