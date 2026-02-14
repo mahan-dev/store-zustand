83b9126c88b2c8ecf706a1a8830a62a3a2744d48
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-14 23:03:10 +0330

    mockedData added-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
+import { Suspense } from "react";
+
 import { mockedData } from "@/api/mockedData";
 import connectDb from "@/helper/mongoDb";
 import Home from "@/templates/Home";
 import { ProductDetailTypes } from "@/types/products/types";
 
 const Page = async () => {
-  await connectDb();
+  // await connectDb();
 
   const data = mockedData as ProductDetailTypes[];
-
-  console.log(data);
 
   if (!data.length) {
     return <h2 className="text-center mt-10">No data has found</h2>;
   }
 
-  return <Home data={data} />;
+  return (
+    <Suspense>
+      <Home data={data} />
+    </Suspense>
+  );
 };
 
 export default Page;
