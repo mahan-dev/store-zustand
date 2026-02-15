4176c85291fa01eb4a6a0594b1a80a8a2679f20a
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-15 22:27:43 +0330

    finalize slider

--- core/components/templates/Swiper.tsx
+++ core/components/templates/Swiper.tsx
@@ -1,4 +1,7 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { BsArrowLeftSquare, BsArrowRightSquare } from "react-icons/bs";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,36 +10,50 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import { mockedData } from "@/core/api/mockedData";
+import { ProductDetailTypes } from "@/core/types/products/types";
 import styles from "@/templates/styles/swiper/route.module.css";
 
 const SwiperSlider = () => {
+  const data = mockedData.slice(0, 6) as ProductDetailTypes[];
+
   return (
     <div className="relative">
-      <BsArrowLeftSquare className="custom-prev text-2xl mx-4 text-black cursor-pointer  p-0 absolute left-2 top-1/2 -translate-y-1/2 z-10" />
-      <BsArrowRightSquare className="custom-next text-2xl mx-4 text-black cursor-pointer  p-0 absolute right-2 top-1/2 -translate-y-1/2 z-10" />
+      <BsArrowLeftSquare className={styles["swiper__arrow-left"]} />
+      <BsArrowRightSquare className={styles["swiper__arrow-right"]} />
 
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation={{
-          nextEl: ".custom-next",
-          prevEl: ".custom-prev",
+          nextEl: `.${styles["swiper__arrow-right"]}`,
+          prevEl: `.${styles["swiper__arrow-left"]}`,
         }}
         pagination={{ clickable: true }}
         spaceBetween={10}
-        slidesPerView={3}
-        className="h-100"
+        slidesPerView={4}
+        className="h-50"
         loop={true}
-        //   autoplay={{
-        //     delay: 1500,
-        //     disableOnInteraction: false,
-        //   }}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
       >
-        <SwiperSlide className={styles["swiper-slide"]}>1</SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>2</SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>3</SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>4</SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>5</SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>6</SwiperSlide>
+        {data.map((item) => (
+          <SwiperSlide key={item.id} className={styles["swiper-slide"]}>
+            <div className=" relative w-40 mx-auto h-40">
+              <Link href={`/product/${item.id}`}>
+                <Image
+                  className=""
+                  src={item.image}
+                  alt={"cardImage"}
+                  fill
+                  sizes="90vw"
+                  priority
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
