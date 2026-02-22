ba170d0f5ea077ac735f635f60115e24c8526a1d
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-22 12:08:40 +0330

    eslint error-4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import { FlatCompat } from "@eslint/eslintrc";
-import importPlugin from "eslint-plugin-import";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,33 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "import/no-unresolved": "error",
-      "import/no-duplicates": "error",
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-          ],
-          "newlines-between": "always",
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-        },
-      ],
-    },
-  },
 ];
 
 export default eslintConfig;
