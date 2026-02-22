e314e3833684a87c511e77184a8bc5f51ff579f3
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-22 12:12:11 +0330

    eslint error-5

--- core/helper/payHandler.ts
+++ core/helper/payHandler.ts
@@ -1,22 +1,19 @@
 "use client";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { toast } from "sonner";
-import type { Position } from "sonner";
 
 interface PayHandlerProps {
   resetStore: () => void;
   router: AppRouterInstance;
 }
 
-const position: Position = "top-center";
-
 export const payHandler = async ({ resetStore, router }: PayHandlerProps) => {
   if (!navigator.onLine) {
-    toast.error("your'e offline ", { position });
+    toast.error("your'e offline ", { position: "top-center" });
     return;
   }
 
-  toast.success("Payment successful", { position });
+  toast.success("Payment successful", { position: "top-center" });
   resetStore();
   await new Promise((resolver) => setTimeout(resolver, 2000));
   router.push("/");
