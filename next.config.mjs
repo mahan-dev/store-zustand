c641dc379c50f781ad033c0014839d679cd58326
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-22 13:18:44 +0330

    pwa fixed-3

--- next.config.mjs
+++ next.config.mjs
@@ -15,27 +15,7 @@ const pwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-
-  runtimeCaching: [
-    {
-      urlPattern: ({ request }) => request.destination === "document",
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "pages",
-        networkTimeoutSeconds: 3,
-      },
-    },
-    {
-      urlPattern: ({ request }) =>
-        ["style", "script", "image", "font"].includes(request.destination),
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "assets",
-      },
-    },
-  ],
+  disable: false, 
 });
-
 
 export default pwaConfig(nextConfig);
