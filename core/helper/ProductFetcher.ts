0b53b5b6b6f14721274882489a585ba141672d8a
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-04-25 19:48:29 +0330

    reset npm registry

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -3,7 +3,6 @@ import { ProductDetailTypes } from "@/types/products/types";
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
   try {
     const url = `https://fakestoreapi.com/products`;
-    console.log("[dataFetcher] Fetching:", url);
 
     const res = await fetch(url, {
       cache: "no-store",
@@ -11,7 +10,6 @@ export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
 
     return res.json();
   } catch (error) {
-    console.error("[dataFetcher] Exception:", error);
     return [];
   }
 };
