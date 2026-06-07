c9ccdad7fedcbc78bb49a601de887a2b0a940903
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-06-07 23:52:30 +0330

    fixed product error-2

--- app/(store)/product/[productId]/page.tsx
+++ app/(store)/product/[productId]/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 import { dataFetcher } from "@/core/helper/ProductFetcher";
 import ProductDetail from "@/templates/ProductDetail";
+import { useQuery } from "@tanstack/react-query";
+import { use } from "react";
 
 interface ProductProps {
   params: Promise<{ productId: string }>;
 }
 
-const ProductPage = async ({ params }: ProductProps) => {
-  const { productId } = await params;
-  const receivedData = await dataFetcher();
+const ProductPage = ({ params }: ProductProps) => {
+  const { productId } = use(params);
+  const { data: storeData } = useQuery({
+    queryKey: ["data fetching"],
+    queryFn: dataFetcher,
+  });
 
   if (isNaN(+productId))
     return <h2 className="text-center mt-12">nothing has found</h2>;
 
-  const data = receivedData[+productId - 1];
+  const data = storeData[+productId - 1];
 
-  if (!data) return <h2>Product Not found</h2>;
+  if (!data) return <h2>Product Not found </h2>;
 
   return <ProductDetail data={data} />;
 };
