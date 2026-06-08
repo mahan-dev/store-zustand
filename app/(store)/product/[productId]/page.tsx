137e3e4024a83b1850fc35201965c138d75e2e38
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-06-08 10:16:38 +0330

    product page changes

--- app/(store)/product/[productId]/page.tsx
+++ app/(store)/product/[productId]/page.tsx
@@ -1,4 +1,6 @@
 import { dataFetcher } from "@/core/helper/ProductFetcher";
+import { UseProduct } from "@/core/hooks/useProduct";
+import { ProductDetailTypes } from "@/core/types/products/types";
 import ProductDetail from "@/templates/ProductDetail";
 
 interface ProductProps {
@@ -7,18 +9,31 @@ interface ProductProps {
 
 const ProductPage = async ({ params }: ProductProps) => {
   const { productId } = await params;
-  const receivedData = await dataFetcher();
+  
+
+  const fetchedData = async (): Promise<ProductDetailTypes[]> => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const data = await fetchedData();
+  const receivedItem = data[+productId - 1];
 
   if (isNaN(+productId))
     return <h2 className="text-center mt-12">nothing has found</h2>;
 
-  const data = receivedData[+productId - 1];
-
-  console.log("data is here " , data)
+  // const data = UseProduct(JSON.parse(JSON.stringify(params)));
 
   if (!data) return <h2>Product Not found</h2>;
 
-  return <ProductDetail data={data} />;
+  return <ProductDetail data={receivedItem} />;
 };
 
 export default ProductPage;
