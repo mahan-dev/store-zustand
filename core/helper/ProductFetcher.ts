766273d8bb3185ae5f8f1fbc6764c6a53f3b1b9d
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-06-08 12:17:51 +0330

    fixed product error-7

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -3,16 +3,22 @@ import { ProductDetailTypes } from "@/types/products/types";
 export const BASE_URL = "https://fakestoreapi.com/";
 
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
+  console.log("hi 1");
   try {
+    console.log("hi 2");
     const res = await fetch(`${BASE_URL}products`, {
       cache: "no-store",
     });
+    console.log("hi 3");
 
     const data = await res.json();
+    console.log("hi 4");
 
     return data;
   } catch (error) {
+    console.log("hi 5");
     console.log("this is the error you looking for", error);
+    console.log("hi 6");
     return [];
   }
 };
