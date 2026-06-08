900dcf1d87325586afed1403ce96e1424685ca6b
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-06-08 11:52:41 +0330

    fixed product error-6

--- app/(store)/product/[productId]/page.tsx
+++ app/(store)/product/[productId]/page.tsx
@@ -10,6 +10,7 @@ const ProductPage = async ({ params }: ProductProps) => {
   const { productId } = await params;
 
   const data = await dataFetcher();
+  if (!data.length) return <h2>Product Not found</h2>;
   const receivedItem = data[+productId - 1];
 
   const res = await fetch(`${BASE_URL}products`, {
@@ -24,8 +25,6 @@ const ProductPage = async ({ params }: ProductProps) => {
 
   if (isNaN(+productId))
     return <h2 className="text-center mt-12">nothing has found</h2>;
-
-  if (!data.length) return <h2>Product Not found</h2>;
 
   return <ProductDetail data={receivedItem} />;
 };
