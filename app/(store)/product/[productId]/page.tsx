3bb92f17f01fd29d30b4b10eff85a9a57374f725
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-06-08 12:45:22 +0330

    fixed product error-14

--- app/(store)/product/[productId]/page.tsx
+++ app/(store)/product/[productId]/page.tsx
@@ -1,4 +1,5 @@
-import { BASE_URL, dataFetcher, dataFetcher2 } from "@/core/helper/ProductFetcher";
+import { mockedData } from "@/core/api/mockedData";
+import { BASE_URL } from "@/core/helper/ProductFetcher";
 
 import ProductDetail from "@/templates/ProductDetail";
 
@@ -7,31 +8,18 @@ interface ProductProps {
 }
 
 const ProductPage = async ({ params }: ProductProps) => {
-  console.log("page start")
   const { productId } = await params;
-  
 
-  // const data = await dataFetcher2();
-  
-  // if (!data.length)
-  //   return <h2 className="w-fit mx-auto mt-12">Product Not found</h2>;
-  // const receivedItem = data[2];
+  const data = mockedData;
 
-  // const res = await fetch(`${BASE_URL}products`, {
-  //   cache: "no-store",
-  // });
+  if (!data.length)
+    return <h2 className="w-fit mx-auto mt-12">Product Not found</h2>;
+  const receivedItem = data[+params - 1];
 
-  // console.log("status:", res.status);
+  if (isNaN(+productId))
+    return <h2 className="w-fit mx-auto mt-12">nothing has found</h2>;
 
-  // if (!res.ok) {
-  //   throw new Error(`Fetch failed: ${res.status}`);
-  // }
-
-  // if (isNaN(+productId))
-  //   return <h2 className="w-fit mx-auto mt-12">nothing has found</h2>;
-
-  // return <ProductDetail data={receivedItem} />;
-  return <h2>hi</h2>
+  return <ProductDetail data={receivedItem} />;
 };
 
 export default ProductPage;
