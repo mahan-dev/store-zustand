9b65f14e8718785895283f8d764589ec80f0feea
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-09-21 22:55:48 +0330

    fix:productFetcher-5

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -17,7 +17,7 @@ const page = async() => {
 
   return (
     <Suspense fallback={<h2 className="text-center mt-10">Loading...</h2>}>
-      <Products data={data} />
+      <Products data={JSON.parse(JSON.stringify(data))} />
     </Suspense>
   );
 };
