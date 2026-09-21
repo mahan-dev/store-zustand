52c9fb522eba382fd18abc5ad33fc5275070f2be
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-09-21 22:36:48 +0330

    fix:productFetcher-2

--- core/helper/ProductFetcher.ts
+++ core/helper/ProductFetcher.ts
@@ -3,21 +3,45 @@ import { ProductDetailTypes } from "@/types/products/types";
 export const BASE_URL = "https://fakestoreapi.com";
 
 export const dataFetcher = async (): Promise<ProductDetailTypes[]> => {
-  try {
+    try {
     const res = await fetch(`${BASE_URL}/products`, {
       cache: "no-store",
     });
-    console.log("fetching");
-    const data = await res.json();
+
+    console.log("STATUS:", res.status);
+    console.log("CONTENT TYPE:", res.headers.get("content-type"));
+
+    const text = await res.text();
+
+    console.log("RESPONSE:", text.slice(0, 500));
 
     if (!res.ok) {
-      throw new Error(`FakeStore API failed: ${res.status}`);
+      throw new Error(`API returned ${res.status}`);
     }
-    console.log("error");
+
+    const data = JSON.parse(text);
 
     return data as ProductDetailTypes[];
   } catch (error) {
-    console.log("this is the error you looking for", error);
-    return [];
+    console.log("REAL FETCH ERROR:", error);
+    throw error; // IMPORTANT: don't return []
   }
+  // try {
+  //   const res = await fetch(`${BASE_URL}/products`, {
+  //     cache: "no-store",
+  //   });
+  //   console.log("fetching");
+  //   const data = await res.json();
+    
+
+  //   if (!res.ok) {
+  //     throw new Error(`FakeStore API failed: ${res.status}`);
+  //   }
+  //   console.log("error");
+
+  //   return data as ProductDetailTypes[];
+  // } catch (error) {
+  //   console.log("this is the error you looking for", error);
+  //   return [];
+  // }
 };
