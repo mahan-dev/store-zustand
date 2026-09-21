68b3bedd76f77f0444b02cc17b5f33824be7b52f
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-09-21 23:02:53 +0330

    fix:productFetcher-6

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -1,15 +1,15 @@
 export const dynamic = "force-dynamic";
 import { Suspense } from "react";
 
-
 import { dataFetcher } from "@/core/helper/ProductFetcher";
 import Products from "@/templates/Products";
+import { mockedData } from "@/core/api/mockedData";
+import { ProductDetailTypes } from "@/core/types/products/types";
 
+const page = async () => {
+  // const receivedData = await dataFetcher()
 
-const page = async() => {
-  const receivedData = await dataFetcher()
-
-  const data = receivedData;
+  const data = mockedData as ProductDetailTypes[];
 
   if (!data.length) {
     return <h2 className="text-center mt-10">No data has found</h2>;
